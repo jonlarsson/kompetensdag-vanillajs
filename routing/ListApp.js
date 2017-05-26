49b3230281d7641fe1e183b2a22da8c7903cef49
Jon Larsson <jontelar@kth.se>
2017-05-26 21:22:58 +0200

    routing with react router

---
+++ routing/ListApp.js
@@ -0,0 +1,25 @@
+(function () {
+  const Master = APP.Master;
+  const Detail = APP.Detail;
+  const Router = ReactRouterDOM.HashRouter;
+  const Route = ReactRouterDOM.Route;
+
+  console.log(!!Master, !!Detail, !!Router, !!Route);
+  class ListApp extends React.Component {
+    constructor(props) {
+      super(props);
+    }
+
+    render() {
+      return (
+          <Router history={ReactRouterDOM.hashHistory}>
+            <div>
+              <Route exact path="/" component={Master}/>
+              <Route path="/detail/:id" component={Detail}/>
+            </div>
+          </Router>
+      );
+    }
+  }
+  APP.ListApp = ListApp;
+})();
