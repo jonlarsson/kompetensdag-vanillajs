74d4d8932456adb54816e8667bed6bf4e2e1a6e5
Jon Larsson <jontelar@kth.se>
2017-05-27 08:04:11 +0200

    basic redux store solution

--- state/setupRedux.js
+++ state/setupRedux.js
@@ -1,1 +1,18 @@
-APP.store = Redux.createStore(APP.countAppReducer);
+
+APP.store = {
+  state: APP.countAppReducer(undefined, {type: "STORE_SETUP"}),
+  eventHandlers: [],
+  subscribe(eventHandler) {
+    this.eventHandlers.push(eventHandler);
+    return () => this.eventHandlers.splice(this.eventHandlers.indexOf(eventHandler), 1);
+  },
+
+  dispatch(action) {
+    this.state = APP.countAppReducer(this.state, action);
+    this.eventHandlers.forEach(eventHandler => eventHandler(this.state));
+  },
+
+  getState() {
+    return this.state;
+  }
+};
